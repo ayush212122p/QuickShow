0804254f9d39dd38441e9ca919dbeadae31f0e6c
ayush212122p <ayushsrivastava388900@gmail.com>
2025-07-21 09:16:34 +0530

    db changes

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -6,6 +6,7 @@ const bookingSchema = new mongoose.Schema({
     amount:{type:Number,required:true},
     bookedSeats:{type:Array,required:true},
     isPaid:{type:Boolean,default:false},
+    paymentLink:{type:String}
 },{timestamps:true})
 
 const Booking = mongoose.model("Booking",bookingSchema);

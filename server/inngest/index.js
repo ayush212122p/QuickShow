e3d1a4948f607baff4633aae8ab1e8b7926caede
ayush212122p <ayushsrivastava388900@gmail.com>
2025-07-22 16:54:56 +0530

    commit some changes in Localestring

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -106,8 +106,8 @@ const sendBookingConfirmationEmail = inngest.createFunction(
    <h2>Hi ${booking.user.name},</h2>
    <p>Your booking for <strong style="color:#F84565;">"${booking.show.movie.title}"</strong> is confirmed.</p>
     <p>
-  <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocalDateString('en-US',{ timeZone:'Asia/Kolkata'})} <br/>
-  <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocalTimeString('en-US',{ timeZone:'Asia/Kolkata'})} <br/>
+  <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-US',{ timeZone:'Asia/Kolkata'})} <br/>
+  <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US',{ timeZone:'Asia/Kolkata'})} <br/>
 </p>
 <p>Enjoy the show! 🍿</p>
 <p>Thanks for booking with us!<br/>- MovieShow Team</p>
